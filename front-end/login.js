8c62ed88d4618cb70dfb86841d37274ed57803ea
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-10-09 09:17:36 -0300

    Validação de senha antes de enviar para o backend

--- front-end/login.js
+++ front-end/login.js
@@ -1,2 +1,22 @@
 "use strict";
 
+const form = document.querySelector("form");
+
+// Verificar dados antes de mandar para o backend
+form.addEventListener("submit", (event) => {
+  const email = document.querySelector("#email").value;
+  const senha = document.querySelector("#password").value;
+
+  // Expressão regular para validar a senha
+  const senhaPadrao =
+    /^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&*])[A-Za-z0-9@#$%^&*]{8,16}$/;
+
+  // Validação da senha
+  if (!senha.match(senhaPadrao)) {
+    alert(
+      "Sua senha deve conter pelo menos 1 letra maiuscula, um numero, um caractere especial e entre 8 à 16 caracteres"
+    );
+    event.preventDefault();
+    return;
+  }
+});
